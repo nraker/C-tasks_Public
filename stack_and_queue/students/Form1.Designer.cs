f54e554a6f57145ca326e0f61a126fe5ef858b4e
nraker <kata0880@gmail.com>
2020-05-17 03:46:08 +0300

    stack_and_queue

---
+++ stack_and_queue/students/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace students
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Extract student";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(174, 38);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(53, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Stack";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(174, 13);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Queue";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "_";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Student list: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 121);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximumSize = new System.Drawing.Size(1000, 160);
+            this.MinimumSize = new System.Drawing.Size(250, 160);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ stack_and_queue/students/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace students
+{
+    public partial class Form1 : Form
+    {
+
+           public Stack<string> student_stack = new Stack<string>();
+           public Queue<string> student_queue = new Queue<string>();
+           public string student_name;
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked && student_stack.Count > 0)
+            {
+                label1.Text = "Last extracted in stack: " + student_stack.Pop(); // Извлекает позицию из стека и возвращает ее в label
+                label2.Text = "Student list: " + string.Join(", ", student_stack.ToArray()); // Показывает все позиции в стеке
+            }
+            else if (radioButton2.Checked && student_queue.Count > 0)
+            {
+                label1.Text = "Last extracted in queue: " + student_queue.Dequeue(); // Извлекает позицию из очереди и возвращает ее в label
+                label2.Text = "Student list: " + string.Join(", ", student_queue.ToArray()); // Показывает все позиции в очереди
+            }
+            else if(radioButton1.Checked && student_stack.Count <= 0)
+            {
+                label1.Text = "No students in stack";
+            }
+            else if (radioButton2.Checked && student_queue.Count <= 0)
+            {
+                label1.Text = "No students in queue";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            student_name = textBox1.Text;
+
+            if (textBox1.TextLength > 0) {
+                if (radioButton1.Checked)
+                {
+                    student_stack.Push(student_name); // Добавляет новую позицию в стек
+                    label1.Text = "You add student " + student_stack.Peek(); // Показывает последнюю позицию
+                    label2.Text = "Student list: " + string.Join(", ", student_stack.ToArray()); // Показывает все позиции в стеке
+                }
+                else if (radioButton2.Checked)
+                {
+                    student_queue.Enqueue(student_name); // Добавляет новую позицию в очередь
+                    label2.Text = "Student list: " + string.Join(", ", student_queue.ToArray()); // Показывает все позиции в очереди
+                }
+                else
+                {
+                    label1.Text = "Choose stack or queue";
+                }
+            }
+            else
+            {
+                label1.Text = "Enter student name";
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            bool letter = char.IsLetter(e.KeyChar);
+            bool control = Char.IsControl(e.KeyChar);
+
+            if (!(letter || control))
+            {
+                e.Handled = true;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
